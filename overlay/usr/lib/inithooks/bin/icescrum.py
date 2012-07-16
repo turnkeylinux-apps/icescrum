0ec94370194fc9121f49ffd846468021efdd56d5
Alon Swartz <alon@turnkeylinux.org>
2012-07-16 14:03:25 +0300

    inithook to set admin password, email and domain to serve

---
+++ overlay/usr/lib/inithooks/bin/icescrum.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+"""Set iceScrum admin password, email and domain to serve
+
+Option:
+    --pass=     unless provided, will ask interactively
+    --email=    unless provided, will ask interactively
+    --domain=   unless provided, will ask interactively
+                DEFAULT=www.example.com
+"""
+
+import sys
+import getopt
+import hashlib
+
+from dialog_wrapper import Dialog
+from mysqlconf import MySQL
+from executil import system
+
+def usage(s=None):
+    if s:
+        print >> sys.stderr, "Error:", s
+    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
+    print >> sys.stderr, __doc__
+    sys.exit(1)
+
+DEFAULT_DOMAIN="www.example.com"
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
+                                       ['help', 'pass=', 'email=', 'domain='])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    email = ""
+    domain = ""
+    password = ""
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+        elif opt == '--pass':
+            password = val
+        elif opt == '--email':
+            email = val
+        elif opt == '--domain':
+            domain = val
+
+    if not password:
+        d = Dialog('TurnKey Linux - First boot configuration')
+        password = d.get_password(
+            "iceScrum Password",
+            "Enter new password for the iceScrum 'admin' account.")
+
+    if not email:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        email = d.get_email(
+            "iceScrum Email",
+            "Enter email address for the iceScrum 'admin' account.",
+            "admin@example.com")
+    
+    if not domain:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        domain = d.get_input(
+            "iceScrum Domain",
+            "Enter the domain to serve iceScrum.",
+            DEFAULT_DOMAIN)
+
+    if domain == "DEFAULT":
+        domain = DEFAULT_DOMAIN
+
+    hash = hashlib.sha256(password).hexdigest()
+
+    m = MySQL()
+    m.execute('UPDATE icescrum.icescrum2_user SET passwd=\"%s\" WHERE username=\"admin\";' % hash)
+    m.execute('UPDATE icescrum.icescrum2_user SET email=\"%s\" WHERE username=\"admin\";' % email)
+
+    config = "/etc/icescrum/config.properties"
+    system("sed -i \"s|serverURL =.*|serverURL = http://%s|\" %s" % (domain, config))
+
+    # restart tomcat if running so changes will take effect
+    try:
+        system("/etc/init.d/tomcat6 status >/dev/null")
+        system("/etc/init.d/tomcat6 restart")
+    except:
+        pass
+
+
+if __name__ == "__main__":
+    main()
+
